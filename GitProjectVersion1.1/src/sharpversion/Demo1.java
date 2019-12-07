418dedb027e459d8c508cb9892523346031a54af
danytyagi <dotnetlambda@gmail.com>
2019-12-07 22:31:14 +0530

    new change

--- GitProjectVersion1.1/src/sharpversion/Demo1.java
+++ GitProjectVersion1.1/src/sharpversion/Demo1.java
@@ -3,8 +3,8 @@ package sharpversion;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		String name="Java";
-		System.out.println(name); 
+		String nameone="Java";
+		System.out.println(nameone); 
 
 	}
 
